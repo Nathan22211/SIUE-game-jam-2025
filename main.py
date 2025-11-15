c06737b35d2ea0ac169205c6783d09b9281a66f7
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-11-14 19:51:03 -0600

    first prototype done

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def result_one():
     (3) Hand him a virtual monitor output device""")
     return result
 
-def prompt_one(score):
+def prompt_one(total_score):
     panel_score = 1
     print("She's crying, hand me the dummy")
     result = result_one()
@@ -20,6 +20,7 @@ def prompt_one(score):
         if result == "1":
             print("Where'd you have the crash test dummy the entire time!?")
             panel_score -= 1
+            total_score -= 1
             result = result_one()
         elif result == "2":
             print("Thank you, hopefully the dog doesn't get her mouth on it.")
@@ -27,13 +28,12 @@ def prompt_one(score):
         elif result == "3":
             print("I don't need a fake monitor right now.")
             panel_score -= 1
+            total_score -= 1
             result = result_one()
         else:
             print("[System] error, valid inputs are 1-3")
             result = result_one()
-    print("You're not help... I'll get it myself. Maybe the dog has it")
-    return panel_score
+    print("You're no help... I'll get it myself. Maybe the dog has it")
+    return total_score + panel_score
 
 prompt_one(total_score)
-            
-            
