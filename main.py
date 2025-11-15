dc18c8112f504b3565210165e88b0106e31fb83f
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-11-15 13:13:28 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -418,7 +418,7 @@ def result_seven_full(order_1,order_2):
 
 def prompt_seven(total_score):
     panel_score = 2
-    correct = random.randint(1,2))
+    correct = random.randint(1,2)
     answers = [[1, ["2","4"],1,1],[1,["2","1"],1,2],[1,["1","1"],2,2],[1,["1","4"],2,1],[2,["1","1"],1,1],[2,["1","4"],1,2],[2,["2","4"],2,2],[2,["2","1"],2,1]]
     if correct == 1:
         print("The date had a weird effect on him")
